ab37a03be7397ad98edde26317a0e9fcca7415fa
Dave Zadok <david.zadok@perkinelmer.com>
2023-03-15 10:24:21 -0400

    autoformat

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,17 +9,17 @@ lsp.ensure_installed({
 
 lsp.nvim_workspace()
 
-local cmp = require ('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ['C-space'] = cmp.mapping.complete(),
+      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+      ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+      ['C-space'] = cmp.mapping.complete(),
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -36,6 +36,7 @@ end)
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
 
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
