69e4324ad77d076c3554a04681f1dd457b9d9342
Dave Zadok <david.zadok@perkinelmer.com>
2023-04-13 14:03:18 -0400

    hi

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,25 +1,25 @@
 local status, lualine = pcall(require, 'lualine')
 
-local nightfly = require('lualine.themes.nightfly')
-
-local new_colors = {
-  blue = '#65D1FF',
-  green = "#3EFFDC",
-  violet = '#FF61EF',
-  yellow = '#FFDA7B',
-  black = '#000000'
-}
-
-nightfly.normal.a.bg = new_colors.blue
-nightfly.insert.a.bg = new_colors.green
-nightfly.visual.a.bg = new_colors.violet
-nightfly.command = {
-  a = {
-    gui = 'bold',
-    bg = new_colors.yellow,
-    fg = new_colors.black
-  },
-}
+-- local nightfly = require('lualine.themes.nightfly')
+--
+-- local new_colors = {
+--   blue = '#65D1FF',
+--   green = "#3EFFDC",
+--   violet = '#FF61EF',
+--   yellow = '#FFDA7B',
+--   black = '#000000'
+-- }
+--
+-- nightfly.normal.a.bg = new_colors.blue
+-- nightfly.insert.a.bg = new_colors.green
+-- nightfly.visual.a.bg = new_colors.violet
+-- nightfly.command = {
+--   a = {
+--     gui = 'bold',
+--     bg = new_colors.yellow,
+--     fg = new_colors.black
+--   },
+-- }
 
 lualine.setup({
   options = {
