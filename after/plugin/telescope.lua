b8c9a74d88a3339c0ac5131cd1fbe5c4051970de
Dave Zadok <david.zadok@perkinelmer.com>
2023-05-25 09:02:15 -0400

    update project files keymap not to conflict with oil

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,7 +9,7 @@ require('telescope').setup {
 }
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
