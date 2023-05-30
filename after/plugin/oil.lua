443dff9bf5bd3fa65f4c7583d0c4fa439a8e9dfc
Dave Zadok <david.zadok@perkinelmer.com>
2023-05-30 08:35:24 -0400

    Show hidden files in oil

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -2,5 +2,11 @@ require("oil").setup({
   default_file_explorer = true,
   prompt_save_on_select_new_entry = true,
   use_default_keymaps = true,
+  buf_options = {
+    buflisted = false,
+  },
+  view_options = {
+    show_hidden = true,
+  }
 })
 vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
