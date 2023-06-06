0ccf921810371065837a17c385871132c8973c39
Dave Zadok <david.zadok@perkinelmer.com>
2023-06-06 12:09:12 -0400

    Setup :make to run eslint

--- lua/commands/init.lua
+++ lua/commands/init.lua
@@ -1,8 +1,10 @@
 local augroup = vim.api.nvim_create_augroup("strdr4605", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "typescript,typescriptreact",
+  pattern = "typescript,typescriptreact,javascript,javascriptreact",
   group = augroup,
-  command = "compiler tsc | setlocal makeprg=npx\\ tsc",
+  command =
+  "setlocal errorformat=%A%f:%l:%c:%m,%-G%.%# | setlocal makeprg=yarn\\ eslint\\ --quiet\\ -f\\ unix",
+
 })
 
 vim.api.nvim_create_user_command('W', 'write', {})
